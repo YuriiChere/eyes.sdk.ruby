3fe3c9bf180e79486fe63a1a28deda9d7eea2fbc
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-03-15 13:19:51 +0200

    Update to save new baselines

--- coverage_tests/configuration/index.js
+++ coverage_tests/configuration/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'eyes_selenium_ruby',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/initialize.js',
+  emitter: '../../../../../coverage_tests/initialize.js',
   overrides: '../../../../../coverage_tests/overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/ruby/template.hbs',
   ext: '_spec.rb',
